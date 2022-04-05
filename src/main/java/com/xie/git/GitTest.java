aa2e3a3234e236d3f5c024b394c9405c648b6157
MrX <MrX@qq.com>
2022-04-05 20:35:13 +0800

    first commit

---
+++ src/main/java/com/xie/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.xie.git;
+
+/**
+ * @author MR-X
+ * @create 2022-04-05 20:34
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
